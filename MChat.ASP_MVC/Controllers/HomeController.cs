5ed7d0b256b54d8ebf197c3b80d1202d4ee74c8a
tobiridi <tony.6141@hotmail.com>
2026-02-19 11:53:28 +0100

    create project

---
+++ MChat.ASP_MVC/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics;
+using MChat.ASP_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MChat.ASP_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MChat.ASP_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MChat.ASP_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
