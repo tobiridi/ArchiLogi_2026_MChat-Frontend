aa5bf64ce906f54bcf7c18d6ed88ab669f8b54a5
tobiridi <tony.6141@hotmail.com>
2026-03-17 10:28:31 +0100

    add overlay component

---
+++ MChat.Client/Components/OverlayModal.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MChat.Client.Components
+{
+    public partial class OverlayModal
+    {
+        [Parameter]
+        public bool IsVisible { get; set; } = true;
+
+        [Parameter]
+        public EventCallback OnClose { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        private async Task CloseOverlay()
+        {
+            await OnClose.InvokeAsync();
+            IsVisible = false;
+        }
+    }
+}
